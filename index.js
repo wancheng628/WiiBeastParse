7bd751af60ddc0e8536fc3ad076d972021b4e5b0
Wancheng Piao <pwc628@gmail.com>
2017-01-31 00:33:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var api = new ParseServer({
         apiKey: 'AIzaSyDeZ08rXk-ynVCxfJK9jHcwNE-Ad0ypZa4'
       },
       ios: {
-        pfx: 'Certificates.p12',
+        pfx: 'wiibeastapnsCertificates.p12',
         passphrase: '', // optional password to your p12/PFX
         bundleId: 'com.wiibeast.WiiBeast',
         production: true
